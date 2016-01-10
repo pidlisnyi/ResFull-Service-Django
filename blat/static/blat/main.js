33214eea1bdbfcf7cc84591ac5e5f0b3438f3fbf
konstantin <Rhfrfpz,hf1301>
2016-01-10 16:29:13 +0200

    implementing angular service request to api

--- blat/static/blat/main.js
+++ blat/static/blat/main.js
@@ -1,7 +1,30 @@
+(function(){
+    angular.module('App', [])
+        .config(function($interpolateProvider){
+            $interpolateProvider.startSymbol('@@').endSymbol('@@');
+});
 
+}());
 
-    function helloWorld(msg){
-
-        alert(msg)
+(function(){
+    function MainCtrl($scope, $http){
+        this.$http = $http;
+        $scope.title = 'First Page';
+        this.getBlats()
 
     }
+
+    MainCtrl.prototype.getBlats = function (){
+
+        this.$http.get('/api/blats/')
+            .success(function(res){
+
+                debugger
+
+            })
+
+    }
+
+    angular.module('App')
+        .controller('MainCtrl', MainCtrl)
+}());
